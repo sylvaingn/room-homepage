c6de9043b8b84c69598345912d61ddd5c0d05fa9
Sylvain Gounon <sylvain.gounon@icloud.com>
2020-10-15 10:50:19 +0200

    init

--- script/script.js
+++ script/script.js
@@ -1,5 +1,33 @@
 $.noConflict();
 jQuery(document).ready(($) => {
 
+    // $('.slick-prev').eq(0).text('<p>OK</p>')
+    /* let prev = document.getElementsByClassName('slick-prev');
+    console.log(prev.innerHTtml); */
+    let count = 0;
+    $('#burger, #cross, #opacity').on('click', () => {
+        count++;
+        if (count % 2 == 1) {
+            $('#burger-wrap').css({ height: '110px' });
+            $('#opacity').css({ visibility: 'visible', opacity: '1' });
+        } else {
+            $('#burger-wrap').css({ height: '0' });
+            $('#opacity').css({ opacity: '0' });
+            setTimeout(() => {
+                $('#opacity').css({ visibility: 'hidden' });
+            }, 500);
+        }
+    })
 
+    $('.slider').slick({
+        mobileFirst: true,
+    })
+    /* $('#previous').on('click', () => {
+        for (let i = 0; i < $('.slide').length; i++) {
+                $('.slide').eq(i).css({ display: 'none' });
+                $('.slide').eq(i - 1).css({ display: 'flex' });
+
+        }
+
+    }) */
 });
